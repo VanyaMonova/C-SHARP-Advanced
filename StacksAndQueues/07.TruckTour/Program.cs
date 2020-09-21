ae8e832cf6de33376d21311030433d97745f365c
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-09-21 19:03:36 +0300

    Add files via upload

---
+++ StacksAndQueues/07.TruckTour/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> circle = new Queue<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                input += $" {i}";
+                circle.Enqueue(input);
+            }
+            var totalFuel = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string currentInfo = circle.Dequeue();
+                var info = currentInfo.Split().Select(int.Parse).ToArray();
+
+                var fuel = info[0];
+                var distance = info[1];
+                totalFuel += fuel;
+                if (totalFuel >= distance)
+                {
+                    totalFuel -= distance;
+                }
+                else
+                {
+                    totalFuel = 0;
+                    i = -1;
+                }
+                circle.Enqueue(currentInfo);
+            }
+            var firstElement = circle.Dequeue().Split();
+            Console.WriteLine(firstElement[2]);
+        }
+    }
+}
