488318b793bace94a46fe3bd77d061ff0a6d44d1
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-03 02:33:34 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/02.SetsOfElements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lengths = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int n = lengths[0];
+            int m = lengths[1];
+
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                first.Add(num);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                second.Add(num);
+            }
+
+            var intersection = first.Intersect(second);
+
+            Console.WriteLine(String.Join(" ", intersection));
+        }
+    }
+}
