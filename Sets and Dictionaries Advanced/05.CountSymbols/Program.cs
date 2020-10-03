9950e33925b4835841c13104be7bf69f4f7e1720
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-04 02:56:57 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/05.CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            SortedDictionary<char, int> occurences = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char current = text[i];
+                if (!occurences.ContainsKey(current))
+                {
+                    occurences.Add(current, 0);
+                }
+                occurences[current]++;
+            }
+
+            foreach (var item in occurences)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
