d0adca7681712c9d9e335a2b3805625d10ed6d82
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-04 15:53:26 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/04.EvenTimes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> occurences = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!occurences.ContainsKey(num))
+                {
+                    occurences.Add(num, 0);
+                }
+
+                occurences[num]++;
+            }
+            foreach (var item in occurences)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
