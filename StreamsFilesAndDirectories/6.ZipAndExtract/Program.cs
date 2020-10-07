83ddd0930ff8742cb74b91abb8c5136bf841e5e3
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-08 00:14:15 +0300

    Add files via upload

---
+++ StreamsFilesAndDirectories/6.ZipAndExtract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _6.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@".\toCopy.png", @"C:\Users\HP\Desktop\Demo\result.zip");
+            ZipFile.ExtractToDirectory(@"C:\Users\HP\Desktop\Demo\result.zip", @"C:\Users\HP\Desktop\Demo\");
+        }
+    }
+}
