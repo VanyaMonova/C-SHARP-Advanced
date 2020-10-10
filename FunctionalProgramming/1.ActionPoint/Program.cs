51586be0f54a6b4af01ee463669db13789629538
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-10 14:31:00 +0300

    Add files via upload

---
+++ FunctionalProgramming/1.ActionPoint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = items => Console.WriteLine(string.Join(Environment.NewLine, items));
+            string[] names = Console.ReadLine().Split().ToArray();
+            print(names);
+        }
+    }
+}
---
+++ FunctionalProgramming/2.KnightsOfHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = name => Console.WriteLine($"Sir " + name);
+            string[] names = Console.ReadLine().Split().ToArray();
+            for (int i = 0; i < names.Length; i++)
+            {
+                string name = names[i];
+                print(name);
+            }
+        }
+    }
+}
