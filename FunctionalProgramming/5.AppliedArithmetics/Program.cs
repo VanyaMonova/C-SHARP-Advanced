9a893d62822e1d5b3e533be5228e34281d413a84
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-11 14:24:44 +0300

    Add files via upload

---
+++ FunctionalProgramming/5.AppliedArithmetics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> addFunc = num => num += 1;
+            Func<int, int> multiplyFunc = num => num *= 2;
+            Func<int, int> subtractFunc = num => num -= 1;
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                if (command == "add")
+                {
+                    numbers = numbers.Select(addFunc).ToArray();
+                }
+                else if (command == "multiply")
+                {
+                    numbers = numbers.Select(multiplyFunc).ToArray();
+                }
+                else if (command == "subtract")
+                {
+                    numbers = numbers.Select(subtractFunc).ToArray();
+                }
+                else if (command == "print")
+                {
+                    Console.WriteLine(string.Join(' ', numbers));
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ FunctionalProgramming/6.ReverseAndExclude/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).Reverse().ToList();
+            int n = int.Parse(Console.ReadLine());
+
+            Func<int, bool> predicate = num => num % n != 0;
+
+            Action<List<int>> printNums = numbers => Console.WriteLine(string.Join(' ', numbers));
+
+            printNums(numbers.Where(predicate).ToList());
+        }
+    }
+}
