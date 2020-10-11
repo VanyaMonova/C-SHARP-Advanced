f5143bab7267dbc9e54124e26865e4f76710254c
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-11 14:21:34 +0300

    Add files via upload

---
+++ FunctionalProgramming/3.CustomMinFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minFunc = inputNums => inputNums.Min();
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(minFunc(nums));
+        }
+    }
+}
---
+++ FunctionalProgramming/4.FindEvensOrOdds/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEvenPredicate = num => num % 2 == 0;
+
+            Action<List<int>> printNums = nums => Console.WriteLine(string.Join(' ', nums));
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            List<int> nums = new List<int>();
+
+            int startNum = input[0];
+            int endNum = input[1];
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                nums.Add(i);
+            }
+
+            string numType = Console.ReadLine();
+            List<int> result = new List<int>();
+
+            if (numType == "even")
+            {
+                result = nums.FindAll(x => isEvenPredicate(x));
+            }
+            else
+            {
+                result = nums.FindAll(x => !isEvenPredicate(x));
+            }
+            printNums(result);
+        }
+    }
+}
