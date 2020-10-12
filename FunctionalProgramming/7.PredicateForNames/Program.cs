40d65b939eb2ab8f69e5bb0de8d2ebf23e7e33b5
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-12 12:05:42 +0300

    Add files via upload

---
+++ FunctionalProgramming/7.PredicateForNames/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _7.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+
+            Func<string, bool> filter = n => n.Length <= nameLength;
+
+            Console.ReadLine().Split().Where(filter).ToList().ForEach(Console.WriteLine);
+
+        }
+    }
+}
