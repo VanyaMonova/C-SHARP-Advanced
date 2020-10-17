0f7e40196a49cac1be7a0cbc7e86e6ebf542b75c
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-17 15:05:51 +0300

    Add files via upload

---
+++ DefiningClasses/04.OpinionPoll/Family.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+        public Person GetOldestMember()
+        {
+            int maxAge = int.MinValue;
+            Person person = null;
+            foreach (var currentPerson in People)
+            {
+                var currentAge = currentPerson.Age;
+
+                if (currentAge > maxAge)
+                {
+                    maxAge = currentAge;
+                    person = currentPerson;
+                }
+            }
+            return person;
+        }
+        public Person[] GetPeople()
+        {
+            var people = People.Where(x => x.Age > 30).OrderBy(x => x.Name).ToArray();
+
+            return people;
+        }
+    }
+}
