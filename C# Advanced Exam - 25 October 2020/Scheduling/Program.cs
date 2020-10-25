41b671f86702ee0f87d8e73e776b35c621b3655e
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-25 22:50:27 +0200

    Add files via upload

---
+++ C# Advanced Exam - 25 October 2020/Scheduling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasks = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var threads = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int toBeKilled = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>(tasks);
+
+            Queue<int> queue = new Queue<int>(threads);
+
+            int killer = 0;
+
+            while (true)
+            {
+                int currTask = stack.Peek();
+                int currThread = queue.Peek();
+
+                if (currTask == toBeKilled)
+                {
+                    killer = currThread;
+                    stack.Pop();
+                    break;
+                }
+
+                if (currThread >= currTask)
+                {
+                    stack.Pop();
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.Dequeue();
+                }
+            }
+            Console.WriteLine($"Thread with value {killer} killed task {toBeKilled}");
+
+            Console.WriteLine(string.Join(' ', queue));
+        }
+    }
+}
