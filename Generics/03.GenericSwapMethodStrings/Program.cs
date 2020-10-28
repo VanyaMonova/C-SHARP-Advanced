d42959898204b66ccfc542483f6c847322480fe4
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-28 16:55:19 +0200

    Add files via upload

---
+++ Generics/03.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.GenericSwapMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                string input = Console.ReadLine();
+                box.Values.Add(input);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int a = indexes[0];
+            int b = indexes[1];
+
+            box.Swap(a, b);
+
+            Console.WriteLine(box);
+        }
+    }
+}
