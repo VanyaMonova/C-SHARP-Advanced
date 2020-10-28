4d6c70a4407e11036ab7de2e3b100d8a233e5388
VanyaMonova <71231287+VanyaMonova@users.noreply.github.com>
2020-10-28 16:52:56 +0200

    Add files via upload

---
+++ Generics/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                Console.WriteLine(box);
+
+            }
+
+        }
+    }
+}
